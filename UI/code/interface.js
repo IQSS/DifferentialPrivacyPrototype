bfcc43ca35a7c2e305fa7e5dee575ef9d280d7aa
tercer <jameshonaker2@gmail.com>
2016-12-06 12:38:54 -0500

    changes for production, xhr header

--- UI/code/interface.js
+++ UI/code/interface.js
@@ -176,7 +176,7 @@ var hostname="";
 var metadataurl = "";
 var ddiurl = "";
 
-var dataverse_available = true;  // When Dataverse repository goes down, or is otherwise unavailable, this is a quick override for searching for metadata by url.
+var dataverse_available = false;  // When Dataverse repository goes down, or is otherwise unavailable, this is a quick override for searching for metadata by url.
 
 // Set the fileid (Dataverse reference number) for dataset to use 
 
@@ -511,6 +511,8 @@ function createCORSRequest(method, url, callback) {
          // CORS not supported.
          xhr = null;
      }
+     
+     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
      return xhr;  
 }
 
