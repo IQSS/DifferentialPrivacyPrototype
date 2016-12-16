2e198de167d6849907f2a5fa7ced23e5ceb5e42f
tercer <jameshonaker2@gmail.com>
2016-12-15 21:41:00 -0500

    change rApache app names to TwoRavens convention for production

--- UI/code/interface.js
+++ UI/code/interface.js
@@ -400,7 +400,7 @@ function talktoR(action, variable, stat) {
    }
 
    console.log(jsonout)
-   urlcall = base+"privateAccuracies";
+   urlcall = base+"privateAccuraciesapp";
    console.log("urlcall out: ", urlcall);
    makeCorsRequest(urlcall, btn, estimateSuccess, estimateFail, jsonout);  
 } 
@@ -489,7 +489,7 @@ function talktoRtwo(btn) {
    }
 
 	console.log(jsonout)
-    urlcall = base+"privateStatistics";
+    urlcall = base+"privateStatisticsapp";
     console.log("urlcall out: ", urlcall);
   
     makeCorsRequest(urlcall, btn, statisticsSuccess, estimateFail, jsonout);
